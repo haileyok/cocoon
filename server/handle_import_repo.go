846e77c35d30d21742f49f210dbf21684510849f
Hailey <me@haileyok.com>
2025-06-06 21:36:02 -0700

    fixes

--- server/handle_import_repo.go
+++ server/handle_import_repo.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"io"
 
+	"github.com/bluesky-social/indigo/repo"
 	"github.com/haileyok/cocoon/blockstore"
 	"github.com/haileyok/cocoon/internal/helpers"
 	"github.com/haileyok/cocoon/models"
@@ -13,7 +14,7 @@ import (
 )
 
 func (s *Server) handleRepoImportRepo(e echo.Context) error {
-	repo := e.Get("repo").(*models.RepoActor)
+	urepo := e.Get("repo").(*models.RepoActor)
 
 	b, err := io.ReadAll(e.Request().Body)
 	if err != nil {
@@ -21,7 +22,8 @@ func (s *Server) handleRepoImportRepo(e echo.Context) error {
 		return helpers.ServerError(e, nil)
 	}
 
-	bs := blockstore.New(repo.Repo.Did, s.db)
+	bs := blockstore.New(urepo.Repo.Did, s.db)
+	r := repo.NewRepo(context.TODO(), urepo.Repo.Did, bs)
 
 	cs, err := car.NewCarReader(bytes.NewReader(b))
 	if err != nil {
@@ -45,6 +47,17 @@ func (s *Server) handleRepoImportRepo(e echo.Context) error {
 		}
 
 		currBlock = next
+	}
+
+	root, rev, err := r.Commit(context.TODO(), urepo.SignFor)
+	if err != nil {
+		s.logger.Error("error committing", "error", err)
+		return helpers.ServerError(e, nil)
+	}
+
+	if err := bs.UpdateRepo(context.TODO(), root, rev); err != nil {
+		s.logger.Error("error updating repo after commit", "error", err)
+		return helpers.ServerError(e, nil)
 	}
 
 	return nil
