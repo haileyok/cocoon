e5eb6467c8cd7bbe92edd122b086cf98689dd8a0
Ed Costello <edc.open@gmail.com>
2025-10-29 20:47:51 +1300

    Implement requestPlcOperationSignature

---
+++ server/handle_identity_request_plc_operation.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/haileyok/cocoon/internal/helpers"
+	"github.com/haileyok/cocoon/models"
+	"github.com/labstack/echo/v4"
+)
+
+func (s *Server) handleIdentityRequestPlcOperationSignature(e echo.Context) error {
+	urepo := e.Get("repo").(*models.RepoActor)
+
+	code := fmt.Sprintf("%s-%s", helpers.RandomVarchar(5), helpers.RandomVarchar(5))
+	eat := time.Now().Add(10 * time.Minute).UTC()
+
+	if err := s.db.Exec("UPDATE repos SET plc_operation_code = ?, plc_operation_code_expires_at = ? WHERE did = ?", nil, code, eat, urepo.Repo.Did).Error; err != nil {
+		s.logger.Error("error updating user", "error", err)
+		return helpers.ServerError(e, nil)
+	}
+
+	if err := s.sendPlcTokenReset(urepo.Email, urepo.Handle, code); err != nil {
+		s.logger.Error("error sending mail", "error", err)
+		return helpers.ServerError(e, nil)
+	}
+
+	return e.NoContent(200)
+}
