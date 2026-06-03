53da97a0b6070628fe4c6ec99cfd30a69eea4423
Merilynn Paisley <31383109+furudean@users.noreply.github.com>
2026-06-02 22:35:08 -0700

    allow localhost oauth clients

--- oauth/client/client.go
+++ oauth/client/client.go
@@ -1,8 +1,38 @@
 package client
 
-import "github.com/lestrrat-go/jwx/v2/jwk"
+import (
+	"net/url"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
+)
 
 type Client struct {
-	Metadata *Metadata
-	JWKS     jwk.Key
+	Metadata          *Metadata
+	JWKS              jwk.Key
+	IsLocalhostClient bool
+}
+
+func (c *Client) IsRedirectURIAllowed(requestedURI string) bool {
+	if c.IsLocalhostClient {
+		ru, err := url.Parse(requestedURI)
+		if err != nil || ru.Scheme != "http" || !isLoopbackHost(ru.Hostname()) {
+			return false
+		}
+		for _, registered := range c.Metadata.RedirectURIs {
+			reg, err := url.Parse(registered)
+			if err != nil {
+				continue
+			}
+			if reg.Hostname() == ru.Hostname() && reg.Path == ru.Path {
+				return true
+			}
+		}
+		return false
+	}
+	for _, uri := range c.Metadata.RedirectURIs {
+		if uri == requestedURI {
+			return true
+		}
+	}
+	return false
 }
